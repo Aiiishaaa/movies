1b14adeaf7a1da8652ee705de20aa957911e35fe
gladiator06m <61495716+gladiator06m@users.noreply.github.com>
2022-11-10 22:07:48 +0100

    sdccs

--- controller/connectdb.php
+++ controller/connectdb.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost','root','','movies');
+$conn = new mysqli('localhost','root','','mymovies');
 
 if ($conn->connect_error) {
    die(" Il y a un probleme de connexion à la base de données ! ".$conn->connect_error);
